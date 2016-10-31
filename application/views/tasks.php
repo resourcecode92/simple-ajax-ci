b0384f1752cbaae608f3e4a2c2a32bea8dc7bc2d
Syahriga Syahrul <rigasprogamer@gmail.com>
2016-10-31 16:59:25 +0700

    change url css n script to url

--- application/views/tasks.php
+++ application/views/tasks.php
@@ -2,8 +2,8 @@
 	<head>
 		<title>Todo List - Bekup</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css')?>"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/font-awesome.min.css')?>"/>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
 	</head>
 	<body>
 
@@ -75,8 +75,8 @@
         
 	</body>
 
-    <script src="<?php echo base_url('assets/js/jQuery-2.1.4.min.js')?>" type="text/javascript"></script> 
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js')?>" type="text/javascript"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script> 
 
      <script type="text/javascript">
             $(function () {
